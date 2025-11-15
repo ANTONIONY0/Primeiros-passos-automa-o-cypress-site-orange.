f299725df2412003261ffdff34f7cdd8f1b847b7
antoniony <antonionyalmeida@outlook.com>
2025-11-14 20:33:27 -0400

    user update scenario added

--- cypress/e2e/userspec.cy.js
+++ cypress/e2e/userspec.cy.js
@@ -1,31 +1,49 @@
 import userData from '../fixtures/users/user-data.json'
 
 describe('orange HRM test', () => {
-  const selectorlist ={
+  const selectorsList ={
     usernameField: "[name='username']",
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
     SectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    dashboardGrid:"orangehrm-dashboard-grid",
-    wrongCredentiaAlert: "[role='alert']"
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]'
+    dashboardGrid:".orangehrm-dashboard-grid",
+    wrongCredentiaAlert: "[role='alert']",
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstnamefield:"[name='firstName']",
+    middlenamefield:"[name='middleName']",
+    lastnamefield:"[name='lastName']",
+    genericfield:".oxd-input--active",
+    datefield:"[placeholder='yyyy-dd-mm']",
+    dateclosebutton:".--close",
+    submitbutton:"[type='submit']",
+    
   }
   
   it.only('user info update sucess', () => {
 
     cy.visit('/auth/login')
-    cy.get(selectorlist.usernameField).type('Admin')
-    cy.get(selectorlist.passwordField).type('admin123')
-    cy.get(selectorlist.loginButton).click()
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
-    cy.get(selectorlist.dashboardGrid)
-    cy.get(selectorlist.myInfoButton).click()
+    cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstnamefield).clear().type('tonytest')
+    cy.get(selectorsList.middlenamefield).clear().type('antonoionytest')
+    cy.get(selectorsList.lastnamefield).clear().type('almeidatest')
+    cy.get(selectorsList.genericfield).eq(3).clear().type('Empdtest')
+    cy.get(selectorsList.genericfield).eq(4).clear().type('OtherIdtest')
+    cy.get(selectorsList.genericfield).eq(5).clear().type('10test')
+    cy.get(selectorsList.genericfield).eq(6).clear().type('2025-11-29')
+    cy.get(selectorsList.dateclosebutton).click()
+    cy.get(selectorsList.submitbutton).eq(0).click()
+    cy.get('.oxd-toast-close')
   })
    it('login - fail', () => {
   cy.visit('/auth/login')
-  cy.get(selectorlist.usernameField).type('Test')
-  cy.get(selectorlist.passwordField).type('Test')
-  cy.get(selectorlist.loginButton).click()
-  cy.get(selectorlist.wrongCredentiaAlert)
+  cy.get(selectorsList.usernameField).type('Test')
+  cy.get(selectorsList.passwordField).type('Test')
+  cy.get(selectorsList.loginButton).click()
+  cy.get(selectorsList.wrongCredentiaAlert)
   })
 })
