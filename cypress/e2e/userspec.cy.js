964d46da748fa04140c5a58039d9b839446c3f60
antoniony <antonionyalmeida@outlook.com>
2025-11-21 23:34:18 -0400

    pages objects update

--- cypress/e2e/userspec.cy.js
+++ cypress/e2e/userspec.cy.js
@@ -2,55 +2,30 @@ import userData from '../fixtures/users/user-data.json'
 import LoginPage from '../pages/loginPage'
 import DashboardPage from '../pages/dashboardPage'
 import MenuPage from '../pages/menuPage'
+import MyInfoPage from '../pages/myInfoPage'
 
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
+const myInfoPage = new MyInfoPage()
 
 describe('orange HRM test', () => {
 
   const selectorsList ={
-    firstnamefield:"[name='firstName']",
-    middlenamefield:"[name='middleName']",
-    lastnamefield:"[name='lastName']",
-    genericfield:".oxd-input--active",
-    datefield:"[placeholder='yyyy-dd-mm']",
-    genericcombobox:".oxd-select-text-input",
-    seconditemcombobox:".oxd-select-dropdown > :nth-child(2)",
-    thirditemcombobox:".oxd-select-dropdown > :nth-child(3)",
-    dateclosebutton:".--close",
-    submitbutton:"[type='submit']",
+    
   }
   
   it.only('user info update Sucess', () => {
     loginPage.accessLoginPage()
     loginPage.loginwithUser(userData.usersucess.username,userData.usersucess.password)
-
     dashboardPage.checkDashboardPage()
-
     menuPage.accessMyInfo()
+    myInfoPage.fillPersonalDetails('firstName','middleName','lastName')
+    myInfoPage.fillEmployeeDetails('EmployeeId','OtherId','10test','2025-29-11')
+    myInfoPage.fillstatus()
+    myInfoPage.saveforme()
     
-    
-    cy.get(selectorsList.firstnamefield).clear().type('tonytest')
-    cy.get(selectorsList.middlenamefield).clear().type('antonoionytest')
-    cy.get(selectorsList.lastnamefield).clear().type('almeidatest')
-    cy.get(selectorsList.genericfield).eq(3).clear().type('Empdtest')
-    cy.get(selectorsList.genericfield).eq(4).clear().type('OtherIdtest')
-    cy.get(selectorsList.genericfield).eq(5).clear().type('10test')
-    cy.get(selectorsList.genericfield).eq(6).clear().type('2025-11-29')
-    cy.get(selectorsList.dateclosebutton).click()
-    cy.get(selectorsList.submitbutton).eq(0).click({force:true})
-    cy.get('.oxd-toast-close')
- 
-
-    
-    cy.get(selectorsList.genericcombobox).eq(0).click()
-    cy.get(selectorsList.seconditemcombobox).click()
-    cy.get(selectorsList.genericcombobox).eq(1).click()
-    cy.get(selectorsList.thirditemcombobox).click()
-    
-    
-   
+     
   })
    it('login - fail', () => {
   cy.visit('/auth/login')
---
+++ cypress/pages/myInfoPage.js
@@ -0,0 +1,50 @@
+class MyInfoPage {
+
+    selectorsList() {
+        const selectors = {
+          firstnamefield:"[name='firstName']",
+          middlenamefield:"[name='middleName']",
+          lastnamefield:"[name='lastName']",
+          genericfield:".oxd-input--active",
+          datefield:"[placeholder='yyyy-dd-mm']",
+          genericcombobox:".oxd-select-text-input",
+          seconditemcombobox:".oxd-select-dropdown > :nth-child(2)",
+          thirditemcombobox:".oxd-select-dropdown > :nth-child(3)",
+          dateclosebutton:".--close",
+          submitbutton:"[type='submit']",
+        }
+
+        return selectors
+    }
+    fillPersonalDetails(firstName,middleName,lastName) {
+        cy.get(this.selectorsList().firstnamefield).clear().type(firstName)
+        cy.get(this.selectorsList().middlenamefield).clear().type(middleName)
+        cy.get(this.selectorsList().lastnamefield).clear().type(lastName)
+    }
+    
+    fillEmployeeDetails(EmployeeId,OtherId,DriversLicense,LicenseExpiryDate) {
+        cy.get(this.selectorsList().genericfield).eq(3).clear().type(EmployeeId)
+        cy.get(this.selectorsList().genericfield).eq(4).clear().type(OtherId)
+        cy.get(this.selectorsList().genericfield).eq(5).clear().type(DriversLicense)
+        cy.get(this.selectorsList().genericfield).eq(6).clear().type(LicenseExpiryDate)
+        cy.get(this.selectorsList().dateclosebutton).click()
+    }
+
+    saveforme() {
+         cy.get(this.selectorsList().submitbutton).eq(0).click({force:true})
+         cy.get('.oxd-toast-close')
+ 
+    }
+
+    fillstatus() {
+        cy.get(this.selectorsList().genericcombobox).eq(0).click()
+        cy.get(this.selectorsList().seconditemcombobox).click()
+        cy.get(this.selectorsList().genericcombobox).eq(1).click()
+        cy.get(this.selectorsList().thirditemcombobox).click()
+    }
+
+   
+    
+}
+
+export default MyInfoPage
